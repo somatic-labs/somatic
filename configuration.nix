4637d5f7f6f194fcc14ffb9e19246869f3458653
Jacob Gadikian <jacobgadikian@gmail.com>
2024-05-09 00:17:58 +0800

    update nix configuration

--- configuration.nix
+++ configuration.nix
@@ -1,23 +1,205 @@
-{ config, pkgs, ... }:
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, ... }:
 
 {
-  imports = [ ];
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ./apple-silicon-support
+    ];
 
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = false;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # Pick only one of the below networking options.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  # Set your time zone.
+  time.timeZone = "Asia/Makassar";
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  networking.wireless.iwd = {
+  enable = true;
+  };
+
+  networking.wireless.iwd.settings = {
+    IPv6 = {
+      Enabled = true;
+    };
+    Settings = {
+      AutoConnect = true;
+    };
+  };
+
+  
+  
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkb.options in tty.
+  # };
+
+  # Enable the X11 windowing system.
+
+  nixpkgs.config.allowUnfree = true;
+
+  
+
+  # Configure keymap in X11
+  # services.xserver.xkb.layout = "us";
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # hardware.pulseaudio.enable = true;
+  # OR
+  
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.faddat = {
+     isNormalUser = true;
+     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+     packages = with pkgs; [
+       tree
+     ];
+   };
+
+
+  
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    go
-    rustc
-    cargo
-    zerotier
-    btop
-    htop
-    vim
-    git
-    screen
-    
-  ];
+     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+     btop
+     htop
+     bitcoin
+     chromium
+     wget
+     fastfetch
+     ipfs
+     firefox
+     git
+     gh
+     bun
+     rustup
+     netmaker
+     netbird
+     go
+     iw
+     vscode
+     peergos
+     vlc
+     p2pvc
+     qtox
+     xfce.catfish
+     xfce.gigolo
+     xfce.orage
+     xfce.xfburn
+     xfce.xfce4-appfinder
+     xfce.xfce4-clipman-plugin
+     xfce.xfce4-cpugraph-plugin
+     xfce.xfce4-dict
+     xfce.xfce4-fsguard-plugin
+     xfce.xfce4-genmon-plugin
+     xfce.xfce4-netload-plugin
+     xfce.xfce4-panel
+     xfce.xfce4-pulseaudio-plugin
+     xfce.xfce4-systemload-plugin
+     xfce.xfce4-weather-plugin
+     xfce.xfce4-whiskermenu-plugin
+     xfce.xfce4-xkb-plugin
+     xfce.xfdashboard
+     xorg.xev
+     zerotierone
+     gcc
+     clang
+     ccache
+   ];
 
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
 
-# YOU CAN CONFIGURE SSH HERE, BUT WE AREN'T THERE YET
-#  services.openssh.enable = true;
-#  users.users.root.openssh.authorizedKeys.keys = [ "your-ssh-public-key" ];
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services = {
+    blueman.enable = true;
+    openssh.enable = true;
+    xserver = {
+      enable = true;
+      displayManager = {
+        lightdm = {
+          enable = true;
+          greeters.slick = {
+            enable = true;
+            theme.name = "Zukitre-dark";
+          };
+        };
+      };
+      desktopManager.xfce.enable = true;
+    };
+
+  
+    pipewire = {
+     enable = true;
+     pulse.enable = true;
+   };
+    zerotierone = {
+    enable = true;
+    joinNetworks = [ "e3918db4832ba1f3" ];
+  };
+  };
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "24.05"; # Did you read the comment?
+
 }
+
